9f151203f23c5d878992933cf05df327e0cce0d4
Kenneth <kenneth.factor@edu.sait.ca>
2023-10-30 23:18:09 -0600

    Creates Functions

--- 3.py
+++ 3.py
@@ -1,3 +1,19 @@
+def add(num1,num2):
+    result = first_number + second_number
+    return result
+
+def minus(num1,num2):
+    result = first_number - second_number
+    return result
+
+def multiply(num1,num2):
+    result = first_number * second_number
+    return result
+
+def divide(num1,num2):
+    result = first_number / second_number
+    return result
+
 # Simple Calculator
 print("Welcome to simple calculator.")
 print("I will add/subtract/multiple/divide any two numbers you provide.")
@@ -9,17 +25,24 @@ operation = input("Would you like to add/subtract/multiple/divide: ")
 first_number = float(first_input)
 second_number = float(second_input)
 
+output = 0
 if operation == "add":
-    result = first_number + second_number
-    print(f"Result: {result}")
+    output = add(first_input, second_input)
+    print(f"Result: {output}")
+    
 elif operation == "subtract":
-    result = first_number - second_number
-    print(f"Result: {result}")
+    output = minus(first_input, second_input)
+    print(f"Result: {output}")
+
 elif operation == "multiply":
-    result = first_number * second_number
-    print(f"Result: {result}")
+    output = multiply(first_input, second_input)
+    print(f"Result: {output}")
+
 elif operation == "divide":
-    result = first_number / second_number
-    print(f"Result: {result}")
+    output = divide(first_input, second_input)
+    print(f"Result: {output}")
+
 else:
     print("Sorry, I do not understand your request.")
+
+
