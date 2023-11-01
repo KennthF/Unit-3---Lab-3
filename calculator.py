eb469fcf8b7c2d4a076b152e63bb01447d7aaa38
Kenneth <kenneth.factor@edu.sait.ca>
2023-11-01 09:48:08 -0600

    Created a module

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def add(num1,num2):
+    result = num1 + num2
+    return result
+
+def minus(num1,num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1,num2):
+    result = num1 * num2
+    return result
+
+def divide(num1,num2):
+    result = num1 / num2
+    return result
+
+
